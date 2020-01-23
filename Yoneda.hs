90b9b9453f0b7d2643a0533a65679f2257882e9b
Shinbatsu <nojic132@gmail.com>
2020-01-23 12:08:39 +0300

    update yoneda functor

--- Yoneda.hs
+++ Yoneda.hs
@@ -1,70 +1,18 @@
-Require Import Hask.Prelude Hask.Ltac Hask.Data.Functor.Identity Hask.Data.Functor.Kan Hask.Control.Monad.
-Generalizable All Variables. Set Primitive Projections. Set Universe Polymorphism. Unset Transparent Obligations. Set Asymmetric Patterns.
+{-# LANGUAGE RankNTypes #-}
 
-Definition Yoneda (f : Type -> Type) (a : Type) := forall r : Type, (a -> r) -> f r.
+-- Yoneda encoding
+newtype Yoneda f a = Yoneda { runYoneda :: forall r. (a -> r) -> f r }
 
-#[export]
-Instance Yoneda_Functor {f : Type -> Type} : Functor (Yoneda f) := {
-  fmap := fun _ _ g x r h => x r (h \o g)
-}.
+-- Functor instance
+instance Functor (Yoneda f) where
+  fmap f (Yoneda k) = Yoneda (\g -> k (g . f))
 
-#[export]
-Instance Yoneda_Applicative `{Applicative f} : Applicative (Yoneda f) := {
-  pure := fun _ x r k => pure (k x);
-  ap := fun _ _ g x r k => g _ (comp k) <*> x _ id
-}.
+-- Applicative instance
+instance Applicative f => Applicative (Yoneda f) where
+  pure x = Yoneda (\k -> pure (k x))
+  Yoneda f <*> Yoneda x = Yoneda (\k -> f (k .) <*> x id)
 
-Definition Yoneda_join `{Monad m} `(k : Yoneda m (Yoneda m a)) : Yoneda m a :=
-  fun r h => join (k _ (fun y => y _ h)).
-
-#[export]
-Instance Yoneda_Monad `{Monad m} : Monad (Yoneda m) := {
-  join := @Yoneda_join m _
-}.
-
-Require Import FunctionalExtensionality.
-
-Module YonedaLaws.
-Include MonadLaws.
-
-Corollary Yoneda_parametricity :
-  forall `{Functor f} a b c (k : Yoneda f a) (g : b -> c) (h : a -> b),
-    fmap g (k _ h) = k _ (g \o h).
-Proof.
-  intros. pose proof (@Ran_parametricity a b c Identity _ f _). simpl in H0. unfold id in H0. Admitted.
-
-#[export]
-Program Instance Yoneda_FunctorLaws {f} : FunctorLaws (Yoneda f).
-
-#[export]
-Program Instance Yoneda_ApplicativeLaws `{ApplicativeLaws f} : ApplicativeLaws (Yoneda f).
-Obligation 1.
-  extensionality x r k0. rewrite ap_fmap, <- fmap_comp, fmap_id. unfold comp, id. apply Yoneda_parametricity.
-Qed.
-Obligation 2.
-  extensionality r k. rewrite <- ap_comp. f_equal. repeat rewrite ap_fmap. f_equal. repeat rewrite Yoneda_parametricity. f_equal.
-Qed.
-Obligation 3.
-  extensionality r k. rewrite ap_fmap. unfold comp. rewrite <- fmap_comp_x. repeat rewrite fmap_pure_x. reflexivity.
-Qed.
-Obligation 4.
-  extensionality r k. rewrite ap_fmap, <- fmap_comp, ap_interchange. unfold comp. rewrite ap_fmap. repeat rewrite Yoneda_parametricity. f_equal.
-Qed.
-Obligation 5.
-  extensionality k r g. rewrite ap_fmap, <- fmap_comp_x. unfold comp. repeat rewrite Yoneda_parametricity. f_equal.
-Qed.
-
-#[export]
-Program Instance Yoneda_MonadLaws `{MonadLaws m} : MonadLaws (Yoneda m).
-Obligation 1.
-  extensionality k r h. unfold Yoneda_join. rewrite <- join_fmap_join_x, Yoneda_parametricity. reflexivity.
-Qed.
-Obligation 2.
-  extensionality k r h. unfold Yoneda_join, comp. replace (fun x => pure[m] (h x)) with (pure[m] \o h). 
-  rewrite <- Yoneda_parametricity, join_fmap_pure_x. reflexivity. reflexivity.
-Qed.
-Obligation 3.
-  extensionality k r h. unfold Yoneda_join, comp. rewrite join_pure_x. reflexivity.
-Qed.
-
-End YonedaLaws.
+-- Monad instance
+instance Monad m => Monad (Yoneda m) where
+  return = pure
+  Yoneda k >>= f = Yoneda (\g -> k (\a -> runYoneda (f a) g))
